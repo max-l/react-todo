c3d2e04ddfa2112d088dad9abcc1c7d5f2be4366
Maxime Lévesque <maxime.levesque@gmail.com>
2023-03-24 11:48:39 -0400

    breaking import

--- app/app.tsx
+++ app/app.tsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from "npm:react";
+import { useEffect, useState, useMemo } from "npm:react";
 import namor from 'npm:namor'
+import { useTable } from 'npm:react-table'
+
 
 
 const range = len => {
@@ -41,7 +43,94 @@ function makeData(...lens) {
   return makeDataLevel()
 }
 
-export default function App() {
-  return <div>
-  </div>
+
+function Table({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data,
+  })
+
+  // Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
 }
+
+function App() {
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Name',
+        columns: [
+          {
+            Header: 'First Name',
+            accessor: 'firstName',
+          },
+          {
+            Header: 'Last Name',
+            accessor: 'lastName',
+          },
+        ],
+      },
+      {
+        Header: 'Info',
+        columns: [
+          {
+            Header: 'Age',
+            accessor: 'age',
+          },
+          {
+            Header: 'Visits',
+            accessor: 'visits',
+          },
+          {
+            Header: 'Status',
+            accessor: 'status',
+          },
+          {
+            Header: 'Profile Progress',
+            accessor: 'progress',
+          },
+        ],
+      },
+    ],
+    []
+  )
+
+  const data = useMemo(() => makeData(20), [])
+
+  return (
+      <Table columns={columns} data={data} />
+  )
+}
+
+export default App
