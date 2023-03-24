63304976d3a58cf185bc0340745458767c426d40
Maxime Lévesque <maxime.levesque@gmail.com>
2023-03-24 11:31:35 -0400

    remove unneeded stuff

--- index.tmpl.ts
+++ index.tmpl.ts
@@ -16,18 +16,9 @@ export default function (_data: unknown, { url }: Helpers) {
 		<head>
 			<meta charset="utf-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-			<title>React • TodoMVC</title>
-			<link rel="stylesheet" href="${url("/styles.css")}">
 		</head>
 		<body>
 			<section class="todoapp" id="app">${ssr}</section>
-			<footer class="info">
-				<p>Double-click to edit a todo</p>
-				<p>
-					Created by <a href="http://github.com/petehunt/">petehunt</a>
-					As a part of <a href="http://todomvc.com">TodoMVC</a>
-				</p>
-			</footer>
 
 			<script type="module" src="${url("/main.js")}" bundle></script>
 		</body>
